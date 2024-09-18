ccf15c3ed29d8c2ff297b6dd15d77be08cf7e9f4
TeDeLimon <tysonpopluis@gmail.com>
2024-09-18 09:40:31 +0200

    Inicialización repositorio

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+
+defineProps({
+    users: {
+        type: Array,
+        default: () => [],
+    },
+});
+</script>
+
+<template>
+    <AppLayout title="Users">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Users
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <!-- <pre>{{ users }}</pre> -->
+                    <div v-for="user in users" :key="user.id">
+                        {{ user.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Models\User;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+    
+    Route::get('/dashboard/users', function () {
+        return Inertia::render('Users/Index', [
+            'users' => User::all(),
+        ]);
+    })->name('users.index');
 });
